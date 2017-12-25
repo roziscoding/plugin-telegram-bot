d9aecaa8335fa8020529b27bd429e17637a8ad6d
Rogério Munhoz <rogerio.j.munhoz@gmail.com>
2017-12-25 11:06:21 -0200

    feat: remove old handler files

--- handlers/text.js
+++
@@ -1,7 +0,0 @@
-module.exports = () => ({
-  type: 'text',
-  handler (msg, result, bot) {
-    if (!result.text) throw new Error(`Not a valid command result. ${result}`)
-    return bot.sendMessage(msg.chat.id, result.text, result.options)
-  }
-})
